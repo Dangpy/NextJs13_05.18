6743fef16bd534c61c36adc4561046671da5eb1b
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-05-18 21:33:45 +0900

    globalCss,font 추가

---
+++ search-profile/src/app/font.ts
@@ -0,0 +1,13 @@
+import { Mogra, Roboto } from "next/font/google";
+
+export const mogra = Mogra({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
