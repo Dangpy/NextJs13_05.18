60648673911931ca7a024a962d599243df666985
MyungJi_Jang <myungvely712@hufs.ac.kr>
2023-05-18 21:09:50 +0900

    home미리 세팅

--- search-profile/src/app/page.tsx
+++ search-profile/src/app/page.tsx
@@ -1,7 +1,53 @@
-import Image from 'next/image'
+// "use client";
+//-> 임포트 링크
+// import styled from "styled-components";
+// import css from "styled-jsx/css";
+
+// const style = css`
+//   h1 {
+//     margin-left: 1.6px;
+//     font-weight: 800;
+//     color: #323433;
+//     text-decoration-line: underline;
+//     text-decoration-style: wavy;
+//     padding: 1rem;
+//     border: 0.2rem solid #323433;
+//     border-radius: 0.9rem;
+//     background-color: #f5f5f5;
+//     box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem #50482e;
+//   }
+//   button {
+//     margin: 0rem 3rem;
+//     padding: 1rem;
+//     border: 0.2rem solid #323433;
+//     border-radius: 0.9rem;
+//     background-color: #f5f5f5;
+//     box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem #50482e;
+//   }
+//   button:hover {
+//     color: #f5f5f5;
+//     background-color: #323433;
+//   }
+// `;
+
+// const SearchGitProfile = styled.main`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+
+//   min-height: 100vh;
+// `;
+
+import Image from "next/image";
 
 export default function Home() {
   return (
+    //   <SearchGitProfile>
+    //   <h1>Do you want to search GitHub username?</h1>
+    //     <button>GO TO SEARCH</button> //Link-> Users로
+    //   <style jsx>{style}</style>
+    // </SearchGitProfile>
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -15,7 +61,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -47,7 +93,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +110,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +127,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +144,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -109,5 +155,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
